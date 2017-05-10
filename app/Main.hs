903f2af73ce9ac821472e9036843b3afd4f78ec5
Raul Ferreira <raulmendesferreira@gmail.com>
2017-05-10 15:00:11 +0100

    Implemented ability to use post requests both to create a game and to join a game. ( Still needs major improvements )
    Had to implement extra functions to deal with lists of tuples.
    
    Imports are better organized too.

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,62 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import           GHC.Generics
 
 import           Web.Spock
 import           Web.Spock.Config
 
 import           Control.Concurrent.MVar
 import           Control.Monad.Trans
-import           Data.Aeson              (FromJSON, Object, ToJSON, decode,
-                                          eitherDecode, encode)
+import           Data.Aeson              (FromJSON, Object, ToJSON,
+                                          eitherDecode, encode) -- decode, parseJSON
 import           Data.IORef
+import           Data.List               (insertBy, deleteBy) --insert
 import           Data.Monoid
+import           Data.Ord                (comparing)
 import           Data.String.Conversions (cs)
 import qualified Data.Text               as T
+
 import           System.Random
 
 import           Game.Data
 import           Game.Funcs
 import           Game.Types
 
+import           Lib
+
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
 type SessionID = Int
-data GameServer = GameServer {
-    gameList :: MVar [(SessionID, GameState)]
-}
+
+data GameServer = GameServer
+    { gameList :: MVar [(SessionID, GameState)]
+    , numberOfPlayersOnServer :: IORef Int
+    }
 
 -- | initialize the server state
 newServer :: IO GameServer
 newServer = do
-  games <- newMVar []
-  return GameServer {gameList = games}
+    games <- newMVar []
+    number <- newIORef 0
+    return GameServer {gameList = games, numberOfPlayersOnServer = number}
+
+newGameState :: Int -> StdGen -> Player -> GameState
+newGameState numP seed initialPlayer = newGameRoom
+        where newGameRoom = GameState { deckState = (allcardsDeck,seed)
+                                      , roundN = 0
+                                      , numPlayers = numP
+                                      , players = [initialPlayer]
+                                      }
+newPlayerState :: PlayerState
+newPlayerState = PlayerState { gameHand = []
+                             , cardsOnTable = []
+                             , wasabi = False
+                             , turn = 0
+                             }
 
 main :: IO ()
 main = do
@@ -41,8 +67,8 @@ main = do
 app :: SpockM () MySession MyAppState ()
 app = do
         gameServer <- liftIO newServer
-        seed <- liftIO getStdGen
-        let initialDeck = (allcardsDeck,seed)
+        seedForDeck <- liftIO getStdGen
+        let initialDeck = (allcardsDeck,seedForDeck)
         currentDeck <- liftIO $ newMVar initialDeck
         get root $
             text "Hello World!"
@@ -55,6 +81,56 @@ app = do
             let (currentBoard, currentState) = shuffleDeck deck
             liftIO $ putMVar currentDeck (currentBoard, currentState)
             text  ("Current state of the Deck: " <> T.pack ( show currentBoard ))
+        post "create" $ do
+            boodyOfRequest <- body
+            liftIO $ print boodyOfRequest
+            let bodyDecoded = eitherDecode $ cs boodyOfRequest :: Either String CreateGameInfo
+            liftIO $ print bodyDecoded
+            case bodyDecoded of
+                Left err       -> text $ T.pack err
+                Right gameInfo -> do
+                          listOfGamesInServer <- liftIO $ takeMVar (gameList gameServer)
+
+                          liftIO $ print listOfGamesInServer
+
+                          let newSessionID = if null listOfGamesInServer
+                                             then 1
+                                             else fst (Prelude.head listOfGamesInServer) + 1
+                          seed <- liftIO getStdGen
+                          let gameRoomID = roomIDtoCreate gameInfo
+                          let playerID = userIDCreating gameInfo
+                          let player = Player {pid = playerID, pname = "LOOOOOL", state = newPlayerState} ---- NEEDS TO BE FIXED!!!! ------
+                          let newGame = newGameState gameRoomID seed player
+                          let serverPlusNewGame = insertBy (comparing fst) (newSessionID,newGame) listOfGamesInServer
+                          liftIO $ putMVar (gameList gameServer) serverPlusNewGame
+                          text "New Game was created"
+
+        post "join" $ do
+          boodyOfRequest <- body
+          liftIO $ print boodyOfRequest
+          let bodyDecoded = eitherDecode $ cs boodyOfRequest :: Either String JoinGameInfo
+          liftIO $ print bodyDecoded
+          case bodyDecoded of
+              Left err       -> text $ T.pack err
+              Right gameInfo -> do
+                        listOfGamesInServer <- liftIO $ takeMVar (gameList gameServer)
+
+                        let playerID = userIDJoinning gameInfo
+                        let roomID = roomIDtoJoin gameInfo
+
+                        let gameToJoin = findBy roomID listOfGamesInServer
+
+                        let serverListTmp = deleteBy ( equalling fst ) (roomID,undefined) listOfGamesInServer
+
+                        let gameToJoinUpdated = gameToJoin
+
+                        let serverPlusUpdatedGame = insertBy (comparing fst) gameToJoinUpdated serverListTmp
+                        seed <- liftIO getStdGen
+
+                        liftIO $ putMVar (gameList gameServer) serverPlusUpdatedGame
+                        text "You've joined the game"
+
+
         post "/login" $ do
             boodyOfRequest <- body
             liftIO $ print boodyOfRequest
@@ -68,3 +144,22 @@ app = do
             --     Just a -> liftIO $ print a
             --
             -- text ("done" <> T.pack ( show t ) )
+
+
+
+data JoinGameInfo = JoinGameInfo { userIDJoinning :: Int
+                                 , roomIDtoJoin   :: Int
+                                 } deriving (Show,Generic)
+
+
+
+data CreateGameInfo = CreateGameInfo { userIDCreating :: Int
+                                     , roomIDtoCreate :: Int
+                                     , howManyPlayers :: Int
+                                     } deriving (Show,Generic)
+
+instance FromJSON JoinGameInfo
+instance ToJSON JoinGameInfo
+
+instance FromJSON CreateGameInfo
+instance ToJSON CreateGameInfo
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
+    ( someFunc,findBy,equalling
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+findBy :: Eq a => a -> [(a, b)] -> (a,b)
+findBy y xs = head $ filter ((==) y . fst) xs
+
+equalling :: (Eq a) => (b -> a) -> b -> b -> Bool
+equalling p x y = (==) (p x) (p y)
